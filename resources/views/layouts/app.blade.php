3643a5198975c2aa04997dfe4efc3bfff9875368
skmirajbn <skmirajbn@gmail.com>
2023-11-11 21:52:41 +0600

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,12 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        {{-- Bladewind --}}
+        <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
+        <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
+
+
         @vite('resources/css/all.min.css')
 
         <!-- Scripts -->
@@ -33,5 +39,10 @@
                 {{ $slot }}
             </main>
         </div>
+
+
+        {{-- Bladewind script --}}
+        <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
+        <script src="//unpkg.com/alpinejs" defer></script>
     </body>
 </html>
