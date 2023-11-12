a46df3d160c288786fff207b151e04add2943b61
Sk Miraj <skmirajbn@gmail.com>
2023-11-12 11:53:47 +0600

    updated

---
+++ database/seeders/parsels.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class parsels extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+
+
+
+
+
+
+
+
+    }
+}
