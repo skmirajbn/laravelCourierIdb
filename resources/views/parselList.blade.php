48d97423a9690375ef5954bbe84f36a55337b91c
Sk Miraj <skmirajbn@gmail.com>
2023-11-12 10:54:04 +0600

    updated

--- resources/views/parselList.blade.php
+++ resources/views/parselList.blade.php
@@ -11,9 +11,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div>
-                    <x-bladewind.dropdown
-                    name="country"
-                    :data="$countries" />
+                    <div class="form-control w-full max-w-full">
+                     
+                        <div class="flex gap-3 py-4 px-6">
+                            <select class=" py-2 px-6 rounded-lg">
+                              <option disabled selected>Select Location</option>
+                              <option>Star Wars</option>
+                              <option>Harry Potter</option>
+                              <option>Lord of the Rings</option>
+                              <option>Planet of the Apes</option>
+                              <option>Star Trek</option>
+                            </select>
+                            <select class="py-2 px-6 rounded-lg">
+                              <option disabled selected>Tracking ID</option>
+                              <option>Star Wars</option>
+                              <option>Harry Potter</option>
+                              <option>Lord of the Rings</option>
+                              <option>Planet of the Apes</option>
+                              <option>Star Trek</option>
+                            </select>
+                            <div class="w-full block"><x-bladewind.datepicker type="range"  /></div>
+                        </div>
+                      
+                      </div>
                 </div>
             </div>
         </div>
